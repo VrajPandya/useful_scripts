cfc013780a0d0bf16df5e67e380c787a1434baad
Vraj Pandya <vrajspandya@gmail.com>
2019-03-21 18:44:24 -0700

    add pavan_13.sql

---
+++ work/pavan_13.sql
@@ -0,0 +1,14 @@
+create table pavan_13(
+str1 text,
+str2 text,
+str3 text,
+str4 text,
+str5 text,
+str6 text,
+str7 text,
+str8 text,
+str9 text,
+str10 text,
+str11 text,
+str12 text,
+str13 text);
