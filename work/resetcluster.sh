ede0fb92e0c6e39f55ec02f1d98f6bc9e741f456
Vraj Pandya <vrajspandya@gmail.com>
2019-04-29 16:05:34 -0700

    create reset scripts

---
+++ work/resetcluster.sh
@@ -0,0 +1,6 @@
+rm -rf data1 data2 dataagg sds
+mkdir data1 data2 dataagg sds
+bin/initdb data1;
+bin/initdb data2;
+bin/initdb dataagg;
+
---
+++ work/resetha.sh
@@ -0,0 +1,5 @@
+rm -rf ha_data1 ha_data2 ha_shared
+mkdir ha_data1 ha_data2 ha_shared
+bin/initdb ha_data1;
+bin/initdb ha_data2;
+
